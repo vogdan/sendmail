7ac9f28acca386c1dbc5f7a412cdf51c765f16c0
Bogdan Dumitrică <dbogdan@gmail.com>
2014-04-07 00:24:48 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,4 +1,3 @@
-
 import sys
 import os
 import logging
@@ -6,19 +5,6 @@ import logging
 LOG_FILE = "_".join(sys.argv[0].strip("./").split(".")) + ".log"
 LOG_FILE_PATH = os.getcwd()
 
-#####################################################
-# Logging HOWTO
-# 1. Do not use print. Use the log object instead by
-# importing it in your .py file:
-# from config import log
-# 1a. Only use print if you want messages to be displayed
-# at the console but not in the log file
-# 2. To log message to console and log file use
-# log.info(message)
-# 3. To log message only to log file (debug messages) use
-# log.debug(message)
-# 4. Warning (log.warning )and error (log.error) messages
-# will be logged to both console and log file.
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 # log to file
