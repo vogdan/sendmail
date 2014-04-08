1a4b1a8dab9b6ea440263cf3e3effb75678351f8
“Bogdan” <dbogdan@gmail.com>
2014-04-08 14:02:04 +0300

    done with erase files script

--- del_old_files.py
+++ del_old_files.py
@@ -11,6 +11,9 @@ import os
 from argparse import ArgumentParser
 from logger import log
 
+MIN_SECS = 60
+HOUR_SECS = MIN_SECS * 60
+DAY_SECS = HOUR_SECS * 24
 
 def parse_cli_opts():
     arg_parser = ArgumentParser(description='''
@@ -18,23 +21,25 @@ Delete old files in the specified dir and it's subdirs,
 excepting the files in the except subdir''')
     arg_parser.add_argument('-a', '--age-limit',
                             help='''File age limit (all files older than AGE days will be deleted)
-File age will be determined by time of last access.
+File age will be determined by time of most recent content modification.
 ''',
                             dest='age',
                             type=int,
                             required=True)
     arg_parser.add_argument('-d', '--parent_dir',
-                            help='Parent dir containing all subdirs to delete from',
+                            help='Absolute path of parent dir containing all subdirs to delete from',
                             dest='parent',
                             metavar='DIR',
                             required=True)
     arg_parser.add_argument('-e', '--except-subdir',
-                            help='Skip files in the specified subdir',
+                            help='''Skip files in the specified subdir.
+Relative path inside parent dir''',
+                            dest = 'exclude',
                             metavar='SUBDIR')
     return  arg_parser.parse_args()
 
 
-def is_old(file, limit):
+def is_old(fname, limit):
     """
     Check if file is older than limit days
 
@@ -48,24 +53,59 @@ def is_old(file, limit):
     :return: True - file is older than limit days
              False - otherwise
     """
-    
-    limit_days_ago = time.time() - limit * 24 * 60 * 60
-    file_age = os.stat(file).st_mtime
-    print'### {}'.format(file)
-    print 'limit {}'.format(limit_days_ago)
-    print 'age   {}'.format(file_age)
-    print 'age_d {}'.format((time.time()-file_age)/(24 * 60 * 60))
+    limit_secs = limit * DAY_SECS
+    limit_days_ago = time.time() - limit_secs
+    file_age = get_file_age(fname, 'secs')    
     if file_age < limit_days_ago:
         return True
     return False
-    
 
 
+def get_file_age(fname, unit='secs'):
+    secs_age = time.time() - os.stat(fname).st_mtime
+    if unit is 'days':
+        divider = DAY_SECS
+    elif unit is 'hours':
+        divider = HOUR_SECS
+    elif unit is 'mins':
+        divider = MIN_SECS
+    else:
+        divider = 1
+
+    return secs_age/divider
+
+        
 def main():
     args = parse_cli_opts()
 
-    print is_old("/home/bogdan/New Text Document.txt", 10);
-    print is_old("/home/bogdan/test.py", 10);
+    log.info('''
+Deleting all files:
+\t-- older than {} days
+\t-- in dir {}
+\t-- except subdir "{}"'''.format(args.age, args.parent, args.exclude))
+
+    for root, subdirs, files in os.walk(args.parent, topdown=False):
+        for name in files:
+            full_name = os.path.join(root, name)
+            log.info("Deleting: {}".format(full_name))
+            if is_old(full_name, args.age):
+                if args.exclude and args.exclude in full_name:
+                    log.info("Skipping - file in exclude subdir '{}'".format(args.exclude))
+                else:
+                    os.remove(full_name)
+                    log.info("Done.")
+            else:
+                log.info("Skipping - file not old enough (age: {} days old)".format(
+                        get_file_age(fname, 'days')))
+            
 
 if __name__ == "__main__":
-    main()
+    log_delimiter = "#"*20 + time.strftime("%a, %d %b %Y %X +0000", 
+                                           time.gmtime()) + "#"*10
+    log.info("\n"*2 + log_delimiter + "\n") 
+    
+    try:
+        main()
+    except Exception, e:
+        log.exception(e)
+
